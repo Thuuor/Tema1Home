72b59d0a883bb01598ff1ee01380842af3aa46d6
thuuo <thuuo@DESKTOP-2RELV9J>
2020-09-29 16:22:57 +0200

    Finished

--- src/Exercicis/Exercici1.kt
+++ src/Exercicis/Exercici1.kt
@@ -6,8 +6,34 @@ import java.io.InputStreamReader
 
 fun main(args: Array<String>) {
 	var f = File.listRoots()[0]
+	var ent = -2
+
+	printList(f)
+
+	while (ent != -1) {
+		println("Introduce el numero del directorio: (escribe -1 para cerrar el programa): ")
+		ent = readLine()!!.toInt() as Int
 
 
+		if (ent == 0) {
+			if (f.getParentFile().exists()) {
+				f = File(f.getParent())
+				printList(f)
+			} else {
+				println("El directorio padre no existe")
+			}
+		} else {
+			if (f.isDirectory() && f.canRead()) {
+				f = File(f.listFiles().elementAt(ent - 1).getPath())
+
+				printList(f)
+			}
+		}
+	}
+}
+
+
+fun printList(f: File) {
 	val string = "Llista de fitxers i directoris del directori " + f
 	println(string)
 	println("-".repeat(string.length))
@@ -21,57 +47,6 @@ fun main(args: Array<String>) {
 		if (e.isDirectory())
 			println(e.getName() + "\t <Directori>")
 		count++
-	}
-
-	println("Introduce el numero del directorio: (escribe -1 para cerrar el programa): ")
-
-	var ent = BufferedReader(InputStreamReader(System.`in`)).readLine()
-	if (ent == "-1") {
-		System.exit(0)
-	} else {
-		if (ent == "0") {
-			var numDir = f.listFiles().sorted()[Integer.parseInt(ent) - 1]
-			f = numDir
-			f = f.getParentFile()
-			if (f.exists()) {
-				val string = "Llista de fitxers i directoris del directori " + f
-				println(string)
-				println("-".repeat(string.length))
-
-				println("0.- Directori pare")
-				var count = 1
-				for (e in f.listFiles().sorted()) {
-					print(count.toString() + ".- ")
-					if (e.isFile())
-						println(e.getName() + "\t " + e.length())
-					if (e.isDirectory())
-						println(e.getName() + "\t <Directori>")
-					count++
-				}
-			} else {
-				println("El directorio padre no existe")
-			}
-		} else {
-			var numDir = f.listFiles().sorted()[Integer.parseInt(ent) - 1]
-			if (numDir.isDirectory() && numDir.canRead()) {
-				f = numDir
-
-				val string = "Llista de fitxers i directoris del directori " + f
-				println(string)
-				println("-".repeat(string.length))
-
-				println("0.- Directori pare")
-				var count = 1
-				for (e in f.listFiles().sorted()) {
-					print(count.toString() + ".- ")
-					if (e.isFile())
-						println(e.getName() + "\t " + e.length())
-					if (e.isDirectory())
-						println(e.getName() + "\t <Directori>")
-					count++
-				}
-			}
-		}
 	}
 }
 
